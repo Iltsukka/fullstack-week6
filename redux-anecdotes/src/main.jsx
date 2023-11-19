6625e246d8e497ea523413e884163303d543410a
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2023-11-19 20:54:29 +0200

    added configureStore, installed redux toolkit

--- redux-anecdotes/src/main.jsx
+++ redux-anecdotes/src/main.jsx
@@ -1,15 +1,16 @@
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
-import { Provider } from 'react-redux'
+/* import { createStore, combineReducers } from 'redux'
+ */import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
-import filterReducer from './reducers/filterReducer'
+/* import reducer from './reducers/anecdoteReducer'
+import filterReducer from './reducers/filterReducer' */
+import store from './store'
 
-const combinedReducer = combineReducers({
+/* const combinedReducer = combineReducers({
   anecdotes: reducer,
   filter: filterReducer
 })
-const store = createStore(combinedReducer)
+const store = createStore(combinedReducer) */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
